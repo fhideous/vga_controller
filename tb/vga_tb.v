570868755011ed3d70e0a4f3492f8d1c3fedd1ba
Kul Vlad <kul.leshiy@yandex.ru>
2021-07-16 10:58:27 +0300

    Feat(tb) добавил tb под главный VGA модуль
    Ничего особенного

---
+++ tb/vga_tb.v
@@ -0,0 +1,49 @@
+`timescale 1ns / 1ps
+
+module vga_tb;
+
+    reg          clk;
+    reg          reset;
+
+    wire         VGA_HS;
+    wire         VGA_VS;
+    wire  [3:0]  VGA_G;    
+    wire  [3:0]  VGA_B;    
+    wire  [3:0]  VGA_R;
+    
+    localparam CLK_SEMIPERIOD = 5;
+    
+    vga uut
+    (
+            .clk(clk),
+            .reset(reset),
+            
+            .VGA_HS(VGA_HS),
+            .VGA_VS(VGA_VS),
+                        
+            .VGA_R(VGA_R),
+            .VGA_B(VGA_B),
+            .VGA_G(VGA_G)
+    );
+  
+    
+    initial begin 
+      clk = 'b0;
+      forever begin
+           #CLK_SEMIPERIOD clk = ~clk;
+      end
+     end
+
+   initial begin 
+     reset = 'b0;
+     #500;
+     reset = 'b1;
+   end
+
+   initial begin 
+
+
+   end
+
+
+endmodule
